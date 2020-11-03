aaeb089a877a6b4748100923e34c18fb7d97b588
Maryfro <memusena@gmail.com>
2020-11-03 13:55:50 +0100

    check braces exercise

--- src/x20200909_exercise/CorrectBraces.java
+++ src/x20200909_exercise/CorrectBraces.java
@@ -1,8 +1,6 @@
 package x20200909_exercise;
 
 import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
 
 public class CorrectBraces {
     //given a String with different braces, check if the order of braces is correct. Every next brace can
@@ -11,27 +9,36 @@ public class CorrectBraces {
     public static void main(String[] args) {
         System.out.println(checkBraces("[{}([[]])]")); //exp. true
         System.out.println(checkBraces("[{}([}[]])])")); //exp. false
-
+        System.out.println(checkBraces("[[[]]]")); //exp. true
+        System.out.println(checkBraces("[[[]}]")); //exp. false
     }
 
     public static boolean checkBraces(String input) {
-        String[] arr = new String[input.length()];
-        //String res = "";
-        for (int i = 0; i < input.length() - 1; i++) {
+        ArrayDeque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < input.length(); i++) {
             if (input.charAt(i) == '[' || input.charAt(i) == '(' || input.charAt(i) == '{') {
-                arr[i] = input.substring(i, i + 1);
-            } else if (arr[i + 1] == "]" || arr[i + 1] == ")" || arr[i + 1] == "}") {
+                deque.add(input.charAt(i));
+            } else if (input.charAt(i) == ']') {
+                if (deque.contains('[')) {
+                    deque.remove('[');
+                }else{
+                    deque.add(']');
+                }
+            } else if (input.charAt(i) == ')') {
+                if (deque.contains('(')) {
+                    deque.remove('(');
+                }else{
+                    deque.add(')');
+                }
+            } else if (input.charAt(i) == '}') {
+                if (deque.contains('{')) {
+                    deque.remove('{');
+                }else {
+                    deque.add('}');
+                }
             }
-            // res += input.substring(i, i + 1);
         }
-        System.out.println(Arrays.toString(arr));
-
-        return false;
+        return deque.isEmpty();
     }
-
-    // Deque<Character> res = new ArrayDeque<Character>();
-    // ArrayDeque<Character>
-    // res = new Deque<Character> ;
-
 }
 
